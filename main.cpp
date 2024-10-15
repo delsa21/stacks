a9edef9d32acf5b6ad9b969b5032ce506e2fec1a
delsa21 <A01644911@tec.mx>
2024-10-15 14:50:38 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+#include "Stack.h"
+using namespace std;
+
+int main() {
+    Stack stack;
+
+    stack.push(30);
+    stack.push(50);
+    stack.push(70);
+
+    cout << "El elemento top: " << stack.top() << endl;
+    stack.pop();
+    cout << "El elemento despues del top: " << stack.top() << endl;
+
+
+    return 0;
+}
